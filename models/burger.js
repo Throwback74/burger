998dc812730a0f140a16dda3557f9164f10e55a8
Throwback74 <corey.slade@gmail.com>
2018-07-24 19:31:03 -0700

    leaving class end of night

---
+++ models/burger.js
@@ -0,0 +1,29 @@
+// Import the ORM to create functions that will interact with the database.
+var orm = require("../config/orm.js");
+
+var burger = {
+  selectAll: function(cb) {
+    orm.selectAll("burgers", function(res) {
+      cb(res);
+    });
+  },
+  // The variables cols and vals are arrays.
+  create: function(val_1, val_2, cb) {
+    orm.create(val_1, val_2, function(res) {
+      cb(res);
+    });
+  },
+  update: function(objColVals, condition, cb) {
+    orm.update("burgers", objColVals, condition, function(res) {
+      cb(res);
+    });
+  }
+  // delete: function(deletedburger, cb) {
+  //   orm.delete(deletedburger, function(res) {
+  //     cb(res);
+  //   });
+  // }
+};
+
+// Export the database functions for the controller (burgersController.js).
+module.exports = burger;
